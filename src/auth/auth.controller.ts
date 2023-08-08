2d2e65b523e84b764127c13a38c66a8bc7b3f0de
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-08-08 17:29:15 +0330

    new [auth module] -> append logics of sign-in/up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDTO } from './dto/sign-up.dto';
+import { ValidationSchema } from '@global/decorator/validation-schema.decorator';
+import { signUpSchema } from './schema/sign-up.schema';
+import { signInSchema } from './schema/sign-in.schema';
+import { SignInDTO } from './dto/sign-in.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  @ValidationSchema(signUpSchema)
+  signUp(@Body() user: SignUpDTO) {
+    return this.authService.signUp(user);
+  }
+
+  @Post('/signin')
+  @HttpCode(200)
+  @ValidationSchema(signInSchema)
+  async signIn(
+    @Body() { email_address, password }: SignInDTO,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const [userInstance, jwtToken] = await this.authService.signIn(
+      email_address,
+      password,
+    );
+
+    response.cookie('access_token', jwtToken);
+
+    return userInstance;
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnprocessableEntityException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { DatabaseProvider } from '../database/database.provider';
+import { SignUpDTO } from './dto/sign-up.dto';
+import { compare, compareSync, hash } from 'bcryptjs';
+import { SALT_LENGTH } from './auth.constants';
+import { SignInDTO } from './dto/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
+import { omit } from 'lodash';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly database: DatabaseProvider) {}
+  constructor(
+    private readonly database: DatabaseProvider,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signUp(newUser: SignUpDTO) {
+    const userInstance = await this.database.user.findUnique({
+      select: {
+        id: true,
+      },
+      where: {
+        email_address: newUser.email_address,
+      },
+    });
+
+    if (userInstance) {
+      throw new UnprocessableEntityException(
+        'The email address already in use',
+      );
+    }
+
+    return await this.database.user.create({
+      select: {
+        id: true,
+        email_address: true,
+        full_name: true,
+        createdAt: true,
+      },
+      data: {
+        ...newUser,
+        password: await hash(newUser.password, SALT_LENGTH),
+      },
+    });
+  }
+
+  async signIn(email_address: string, password: string) {
+    const user = await this.database.user.findUnique({
+      select: {
+        id: true,
+        full_name: true,
+        email_address: true,
+        password: true,
+      },
+      where: {
+        email_address,
+      },
+    });
+
+    if (user && compareSync(password, user.password)) {
+      return [
+        omit(user, ['password']),
+        this.jwtService.sign({ id: user.id }, { expiresIn: '1D' }),
+      ];
+    } else {
+      throw new UnauthorizedException(
+        'The Email address or password is incorrect',
+      );
+    }
+  }
 }
