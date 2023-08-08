f699934f40445f83de4abad30aa4c8ce2ea0d590
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-08-08 20:39:43 +0330

    fix [hall module] -> change name of remove hall schema

--- src/halls/schema/remove-hall.schema.ts
+++ src/halls/schema/remove-hall.schema.ts
@@ -1,6 +1,6 @@
 import * as Joi from 'joi';
 import { RemoveHallDTO } from '../dto/remove-hall.dto';
 
-export const createHallSchema = Joi.object<RemoveHallDTO>({
+export const removeHallSchema = Joi.object<RemoveHallDTO>({
   id: Joi.string().trim().uuid({ version: 'uuidv4' }),
 }).options({ presence: 'required' });
