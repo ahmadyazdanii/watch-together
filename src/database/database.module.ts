064da68ac25da5eb4be053553b09625c5430c688
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-08-10 18:34:54 +0330

    update [database module] -> change to dynamic module and support prisma options

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,22 @@
-import { Global, Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { DatabaseProvider } from './database.provider';
+import { PrismaClientOptions } from '@prisma/client/runtime/library';
 
-@Global()
-@Module({
-  providers: [DatabaseProvider],
-  exports: [DatabaseProvider],
-})
-export class DatabaseModule {}
+@Module({})
+export class DatabaseModule {
+  static forRoot(config: PrismaClientOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      global: true,
+      providers: [
+        {
+          provide: DatabaseProvider,
+          useFactory: () => {
+            return new DatabaseProvider(config);
+          },
+        },
+      ],
+      exports: [DatabaseProvider],
+    };
+  }
+}
--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,9 +1,26 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DatabaseProvider extends PrismaClient implements OnModuleInit {
+  private readonly logger: Logger = new Logger(DatabaseProvider.name);
+
   async onModuleInit() {
     await this.$connect();
+
+    this.$on(
+      'query' as never,
+      (event: {
+        timestamp: Date;
+        query: string;
+        params: string;
+        duration: number;
+        target: string;
+      }) => {
+        this.logger.debug(
+          `the query "${event.query}" executed with ${event.params} params on ${event.duration} ms duration`,
+        );
+      },
+    );
   }
 }
