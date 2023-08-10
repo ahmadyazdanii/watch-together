cc1486eb1273ae5f1875f8942844be6699cd56a8
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-08-10 18:33:41 +0330

    update [validation pipe] -> support multi schemas and skip basic types

--- src/global/decorator/validation-schema.decorator.ts
+++ src/global/decorator/validation-schema.decorator.ts
@@ -2,6 +2,10 @@ import { UsePipes } from '@nestjs/common';
 import { JoiValidationPipe } from '../pipe/validation.pipe';
 import { Schema } from 'joi';
 
-export function ValidationSchema(schema: Schema) {
-  return UsePipes(new JoiValidationPipe(schema));
+function ValidationSchema(schemas: Record<string, Schema>);
+function ValidationSchema(schema: Schema);
+function ValidationSchema(inputSchema: Schema | Record<string, Schema>) {
+  return UsePipes(new JoiValidationPipe(inputSchema));
 }
+
+export { ValidationSchema };
--- src/global/pipe/validation.pipe.ts
+++ src/global/pipe/validation.pipe.ts
@@ -2,19 +2,31 @@ import {
   PipeTransform,
   Injectable,
   UnprocessableEntityException,
+  ArgumentMetadata,
+  Type,
 } from '@nestjs/common';
 import { Schema } from 'joi';
 
 @Injectable()
 export class JoiValidationPipe implements PipeTransform {
-  constructor(private readonly schema: Schema) {}
+  constructor(private readonly schemaInput: Schema | Record<string, Schema>) {}
 
-  transform(value: any) {
+  transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
     if (!value) {
       throw new UnprocessableEntityException('Required field(s) not provided');
     }
 
-    const validationResult = this.schema.validate(value, {
+    const schema: Schema = Object.getOwnPropertyNames(
+      this.schemaInput,
+    ).includes(metatype.name)
+      ? this.schemaInput[metatype.name]
+      : this.schemaInput;
+
+    const validationResult = schema.validate(value, {
       abortEarly: true,
     });
 
@@ -23,5 +35,10 @@ export class JoiValidationPipe implements PipeTransform {
     }
 
     return validationResult.value;
+  }
+
+  private toValidate(metatype: Type): boolean {
+    const types: Type[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
   }
 }
