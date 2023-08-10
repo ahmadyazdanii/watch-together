7218462442f25abf31cc332db2b6c859a8651284
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2023-08-10 18:40:41 +0330

    update [hall module] -> complete all routes and services of hall

--- src/halls/halls.controller.ts
+++ src/halls/halls.controller.ts
@@ -1,7 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  ParseIntPipe,
+  HttpStatus,
+} from '@nestjs/common';
 import { HallsService } from './halls.service';
+import { CreateHallDTO } from './dto/create-hall.dto';
+import { ValidationSchema } from '@global/decorator/validation-schema.decorator';
+import { createHallSchema } from './schema/create-hall.schema';
+import { updateHallSchema } from './schema/update-hall.schema';
+import { UpdateHallDTO } from './dto/update-hall.dto';
+import { CurrentUser } from '@global/decorator/current-user.decorator';
 
 @Controller('halls')
 export class HallsController {
   constructor(private readonly hallsService: HallsService) {}
+
+  @Get('/')
+  getHalls(@CurrentUser('id') user_id: string) {
+    return this.hallsService.getHalls(user_id);
+  }
+
+  @Post('/')
+  @ValidationSchema(createHallSchema)
+  createHall(
+    @Body() { movie_url }: CreateHallDTO,
+    @CurrentUser('id') user_id: string,
+  ) {
+    return this.hallsService.createHall(movie_url, user_id);
+  }
+
+  @Put('/:hall_id')
+  @ValidationSchema(updateHallSchema)
+  updateHall(
+    @Param(
+      'hall_id',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      }),
+    )
+    hall_id: number,
+    @Body() updatableHallData: UpdateHallDTO,
+  ) {
+    return this.hallsService.updateHall(hall_id, updatableHallData);
+  }
+
+  @Delete('/:hall_id')
+  removeHall(
+    @Param(
+      'hall_id',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      }),
+    )
+    hall_id: number,
+  ) {
+    return this.hallsService.removeHall(hall_id);
+  }
 }
--- src/halls/halls.service.ts
+++ src/halls/halls.service.ts
@@ -1,7 +1,81 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseProvider } from '../database/database.provider';
+import { UpdateHallDTO } from './dto/update-hall.dto';
+import { map } from 'lodash';
 
 @Injectable()
 export class HallsService {
   constructor(private readonly database: DatabaseProvider) {}
+
+  async getHall(id: number) {
+    const hall = await this.database.hall.findUnique({
+      include: {
+        members: {
+          select: {
+            role: true,
+            member: {
+              select: {
+                id: true,
+                full_name: true,
+              },
+            },
+          },
+        },
+      },
+
+      where: {
+        id,
+      },
+    });
+
+    return {
+      ...hall,
+      members: map(hall.members, (member) => {
+        return { ...member.member, role: member.role };
+      }),
+    };
+  }
+
+  async getHalls(user_id: string) {
+    return await this.database.hall.findMany({
+      where: {
+        members: {
+          some: {
+            member_id: user_id,
+          },
+        },
+      },
+    });
+  }
+
+  async createHall(movie_url: string, user_id: string) {
+    return await this.database.hall.create({
+      data: {
+        movie_url,
+        members: {
+          create: {
+            member_id: user_id,
+            role: 'PROVIDER',
+          },
+        },
+      },
+    });
+  }
+
+  async updateHall(id: number, updatable_data: UpdateHallDTO) {
+    return await this.database.hall.update({
+      where: {
+        id,
+      },
+      data: updatable_data,
+    });
+  }
+
+  async removeHall(id: number) {
+    return await this.database.hall.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
